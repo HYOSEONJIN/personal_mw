7d03bcc1a017699ce34def352aee9eb2b044cd56
HYOSEONJIN <hyoseonjin1@gmail.com>
2021-01-29 17:24:11 +0900

    모달창 태그

--- index/ootd.js
+++ index/ootd.js
@@ -14,13 +14,3 @@ function ootdMain() {
 
 }
 // 모달 팝업 띄울 시 발생하는 이벤트 (이벤트명 : show.bs.modal)
-
-$('#exampleModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-    var what = button.data('what');
-   
-    // 모달 팝업에 데이터 집어넣기 
-
-    var modal = $(this);
-    modal.find('.modal-title').text('New message to ' + what) modal.find('.modal-body input').val(what)
-})
